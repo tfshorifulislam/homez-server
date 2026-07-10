947632f99f3b6474966a4ecdef26629d9112c22a
tfshorifulislam <tfshorifulislam@gmail.com>
2026-07-11 01:07:59 +0600

    backend pagination done

--- index.ts
+++ index.ts
@@ -39,18 +39,34 @@ async function run(): Promise<void> {
       res.send("Server is running!");
     });
 
-    // ======================  All property ==========================
+    // ======================  All property by query ==========================
     app.get('/api/all-properties', async (req: Request, res: Response) => {
-      
+
       const query: Record<string, unknown> = {};
 
       if (req.query.isActive) {
         query.isActive = req.query.isActive;
       }
-      const cursor = collectionallproperty.find(query).sort({ createdAt: -1 })
+
+      const page = Number(req.query.page) || 1;
+      const limit = Number(req.query.limit) || 8;
+
+      const skip = (page - 1) * limit;
+      const total = await collectionallproperty.countDocuments(query);
+
+      const cursor = collectionallproperty
+        .find(query)
+        .sort({ createdAt: -1 })
+        .skip(skip)
+        .limit(limit)
 
       const result = await cursor.toArray();
-      res.send(result);
+      res.send({
+        data: result,
+        total,
+        currentPage: page,
+        totalPages: Math.ceil(total / limit),
+      });
     })
 
 
