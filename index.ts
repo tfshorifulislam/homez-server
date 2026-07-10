fd721da63824cd3771f4133669ac935460b4ffea
tfshorifulislam <tfshorifulislam@gmail.com>
2026-07-10 16:34:11 +0600

    backend successfully run

--- index.ts
+++ index.ts
@@ -1,15 +1,56 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { MongoClient, ServerApiVersion, ObjectId } from "mongodb";
+
+dotenv.config();
 
 const app = express();
+const port: number = Number(process.env.PORT) || 5000;
 
+app.use(cors());
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Homez API Running 🚀");
+// ================= MONGODB =================
+const uri = process.env.MONGODB_CONNECTION!;
+
+if (!uri) {
+  throw new Error("MONGODB_CONNECTION is missing in .env");
+}
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-const PORT = process.env.PORT || 5000;
+// ================= MAIN RUN =================
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+async function run(): Promise<void> {
+  try {
+    // await client.connect();
+
+    const db = client.db("homez");
+    const collectionallitems = db.collection('all-items');
+
+    // ================= HOME =================
+    app.get("/", (req: Request, res: Response) => {
+      res.send("Server is running!");
+    });
+
+
+
+
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+run().catch(console.error);
+
+app.listen(port, () => {
+  console.log(`🚀 Server running on http://localhost:${port}`);
 });
